5ad7ea55ccade71904e959b9d5c64a32cf8415a0
Kate <katyapopovska08@gmail.com>
2024-03-26 12:26:47 +0200

    Add dbContext

---
+++ DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { 
+        } 
+
+        public DbSet<ConfigItem> ConfigItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ConfigItem>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.Name).IsRequired();
+                entity.Property(x => x.Value).IsRequired();
+                entity.HasOne(x => x.Parent)
+                .WithMany(x => x.Childrens)
+                .HasForeignKey(x => x.ParentId)
+                .IsRequired(false)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+    }
+}
---
+++ DAL/Entities/ConfigItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class ConfigItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public int? ParentId { get; set; }
+        public ConfigItem? Parent { get; set; }
+        public List<ConfigItem> Childrens { get; set; } = new List<ConfigItem>();
+
+    }
+}
